9699124c2b3311fad18fbd2334f9f3d928ccb25f
Mitchell Atlas <xatlasm@gmail.com>
2022-12-02 09:17:00 +0900

    refactor

--- day1/src/index.ts
+++ day1/src/index.ts
@@ -2,17 +2,16 @@ import * as fs from "fs/promises";
 
 // part 1
 const input = await fs.readFile("src/input.txt", { encoding: "utf8" });
-
 const sums = input
   .split("\n\n")
   .map((group) => group.split("\n").map((str) => Number(str)))
   .map((group) => group.reduce((sum, val) => sum + val, 0));
-
 const maxSum = Math.max(...sums);
 console.log(`Part 1 solution: ${maxSum}`);
 
 // part 2
 const sortedSums = sums.sort((a, b) => b - a);
-const topThreeSums = sortedSums.slice(0, 3);
-const sumOfTopThreeSums = topThreeSums.reduce((res, sum) => res + sum, 0);
+const sumOfTopThreeSums = sortedSums
+  .slice(0, 3)
+  .reduce((res, sum) => res + sum, 0);
 console.log(`Part 2 solution: ${sumOfTopThreeSums}`);
