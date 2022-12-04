5bfe1f7dcd6aee72baeec3b68a728f4b91d0ec9b
Mitchell Atlas <xatlasm@users.noreply.github.com>
2022-12-04 15:14:06 +0900

    Update create_new_day.sh

--- create_new_day.sh
+++ create_new_day.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-npm init -y --scope @aoc-2022 -w ./packages/"$1"
-cp -r ./packages/day1/. ./packages/"$1"
-sed -i "s/day1/$1/g" ./packages/"$1"/package.json
-sed -i "s/Day 1/Day ${1: -1}/g" ./packages/"$1"/readme.md
-rm -rf ./packages/"$1"/src/input.txt ./packages/"$1"/dist/ ./packages/"$1"/tsconfig.tsbuildinfo
-touch ./packages/"$1"/src/input.txt
+npm init -y --scope @aoc-2022 -w ./packages/"$1" # init the workspace
+cp -r ./packages/day1/. ./packages/"$1" # copy the files from day1
+sed -i "s/day1/$1/g" ./packages/"$1"/package.json # rename inside package.json
+sed -i "s/Day 1/Day ${1: -1}/g" ./packages/"$1"/readme.md # rename inside readme.md
+rm -rf ./packages/"$1"/src/input.txt ./packages/"$1"/dist/ ./packages/"$1"/tsconfig.tsbuildinfo # remove old input.txt and build artifacts
+touch ./packages/"$1"/src/input.txt # make fresh input.txt
 npm install
