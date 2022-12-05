b86076334ea172668b0b788950d39c2cbb1a39f5
Mitchell Atlas <mitchell.atlas@reaktor.com>
2022-12-05 16:08:57 +0900

    update bash for 2 digit days

--- create_new_day.sh
+++ create_new_day.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
+# creates new workspace when passed name param such as: day12
+
 npm init -y --scope @aoc-2022 -w ./packages/"$1" # init the workspace
 cp -r ./packages/day1/. ./packages/"$1"          # copy the files from day1
 OS=$(uname)
+DAY_NUM=$(expr "$1" : "[^0-9]*\([0-9]*\)")
 if [ "$OS" = 'Darwin' ]; then
   # for MacOS
   gsed -i "s/day1/$1/g" ./packages/"$1"/package.json         # rename inside package.json
-  gsed -i "s/Day 1/Day ${1: -1}/g" ./packages/"$1"/readme.md # rename inside readme.md
+  gsed -i "s/Day 1/Day $DAY_NUM/g" ./packages/"$1"/readme.md # rename inside readme.md
 else
   # for Linux and Windows
   sed -i "s/day1/$1/g" ./packages/"$1"/package.json         # rename inside package.json
-  sed -i "s/Day 1/Day ${1: -1}/g" ./packages/"$1"/readme.md # rename inside readme.md
+  sed -i "s/Day 1/Day $DAY_NUM/g" ./packages/"$1"/readme.md # rename inside readme.md
 fi
 
 rm -rf ./packages/"$1"/src/input.txt ./packages/"$1"/dist/ ./packages/"$1"/tsconfig.tsbuildinfo # remove old input.txt and build artifacts
