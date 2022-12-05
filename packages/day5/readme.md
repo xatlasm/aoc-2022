8e26a0eb5ae5e765f2c30c6fff5cc899e4fb8b29
Mitchell Atlas <mitchell.atlas@reaktor.com>
2022-12-05 16:09:29 +0900

    day5 init

---
+++ packages/day5/readme.md
@@ -0,0 +1,7 @@
+# AOC 2022: Day 5
+
+## Run
+
+```sh
+npm run start
+```
---
+++ packages/day5/src/index.ts
@@ -0,0 +1,16 @@
+import * as fs from 'fs/promises';
+
+// part 1
+const input = await fs.readFile('src/input.txt', { encoding: 'utf8' });
+const sums = input
+  .split('\n\n')
+  .map((group) => group.split('\n').reduce((sum, val) => sum + Number(val), 0));
+const maxSum = Math.max(...sums);
+console.log(`Part 1 solution: ${maxSum}`);
+
+// part 2
+const sortedSums = sums.sort((a, b) => b - a);
+const sumOfTopThreeSums = sortedSums
+  .slice(0, 3)
+  .reduce((res, sum) => res + sum, 0);
+console.log(`Part 2 solution: ${sumOfTopThreeSums}`);
