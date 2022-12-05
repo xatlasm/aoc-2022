fc4e6f54fcac3e503a7108ae8700326ea55108b7
Mitchell Atlas <mitchell.atlas@reaktor.com>
2022-12-05 15:07:20 +0900

    update script for mac support

--- create_new_day.sh
+++ create_new_day.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
+
 npm init -y --scope @aoc-2022 -w ./packages/"$1" # init the workspace
-cp -r ./packages/day1/. ./packages/"$1" # copy the files from day1
-sed -i "s/day1/$1/g" ./packages/"$1"/package.json # rename inside package.json
-sed -i "s/Day 1/Day ${1: -1}/g" ./packages/"$1"/readme.md # rename inside readme.md
+cp -r ./packages/day1/. ./packages/"$1"          # copy the files from day1
+OS=$(uname)
+if [ "$OS" = 'Darwin' ]; then
+  # for MacOS
+  gsed -i "s/day1/$1/g" ./packages/"$1"/package.json         # rename inside package.json
+  gsed -i "s/Day 1/Day ${1: -1}/g" ./packages/"$1"/readme.md # rename inside readme.md
+else
+  # for Linux and Windows
+  sed -i "s/day1/$1/g" ./packages/"$1"/package.json         # rename inside package.json
+  sed -i "s/Day 1/Day ${1: -1}/g" ./packages/"$1"/readme.md # rename inside readme.md
+fi
+
 rm -rf ./packages/"$1"/src/input.txt ./packages/"$1"/dist/ ./packages/"$1"/tsconfig.tsbuildinfo # remove old input.txt and build artifacts
-touch ./packages/"$1"/src/input.txt # make fresh input.txt
+touch ./packages/"$1"/src/input.txt                                                             # make fresh input.txt
 npm install
